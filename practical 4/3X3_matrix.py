7c983ec018f008ebbf0fec58ffd76fad87cd48f9
tanishqtelute <tanishqtelute.cse24@sbjit.edu.in>
2026-04-08 13:52:05 +0530

    Add files via upload

---
+++ practical 4/3X3_matrix.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+matrix1 = np.random.randint(1, 10, size=(3, 3))
+matrix2 = np.random.randint(1, 10, size=(3, 3))
+
+print("Matrix 1:")
+print(matrix1)
+
+print("\nMatrix 2:")
+print(matrix2)
+
+addition = matrix1 + matrix2
+print("\nMatrix Addition:")
+print(addition)
+
+multiplication = np.dot(matrix1, matrix2)
+print("\nMatrix Multiplication:")
+print(multiplication)
---
+++ practical 4/4X4_matrix.py
@@ -0,0 +1,6 @@
+import numpy 
+
+identity_matrix = numpy.identity(4)
+
+print("4x4 Identity Matrix:")
+print(identity_matrix)
